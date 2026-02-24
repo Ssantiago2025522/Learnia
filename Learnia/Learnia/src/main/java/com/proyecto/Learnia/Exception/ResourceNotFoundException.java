baa9176ba5ecac08dfff257cc2112b4107dba5e8
hanselgz <hgarcia-2025122@kinal.edu.gt>
2026-02-24 08:54:36 -0600

    se elimino igual paquete Exception con sus clases y la conexiona db

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.Learnia.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
