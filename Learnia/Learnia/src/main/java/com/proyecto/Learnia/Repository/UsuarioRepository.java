e280560c10dde00043bce8428bb10e658fffbec4
Ssantiago-2025522 <Ssantiago-2025522@kinal.edu.gt>
2026-02-24 08:30:39 -0600

    Modificacion de archivos

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/Repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.Learnia.Repository;
-
-import com.proyecto.Learnia.Entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
