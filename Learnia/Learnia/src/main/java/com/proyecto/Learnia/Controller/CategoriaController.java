76de35768a3cf8f70d5cc4c5bc989562e447b224
MarioHernandez-max <mhernandez-2025095@kinal.edu.gt>
2026-02-25 10:21:21 -0600

    Agregar anotaciones y validaciones en entidad

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/Controller/CategoriaController.java
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/Controller/CategoriaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/categorias")
+@RequestMapping("api/categorias")
 public class CategoriaController {
 
     @Autowired
--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/Entity/Categoria.java
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/Entity/Categoria.java
@@ -2,9 +2,17 @@ package com.proyecto.Learnia.Entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Entity
 @Table(name = "categoria")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,36 +20,12 @@ public class Categoria {
     private Integer idCategoria;
 
     @NotBlank(message = "El nombre no puede ir vacio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre {min} y {max} caracteres")
     @Column(name = "nombre")
     private String nombreCategoria;
 
     @NotBlank(message = "La descripcion no puede ir vacia")
-    @Column(name = "descripcion")
+    @Column(name = "descripcion", length = 255, nullable = false)
+    @Size(max = 255, message = "La descripción no puede exceder los 255 caracteres")
     private String descripcionCategoria;
-
-    public Integer getIdCategoria() {
-        return idCategoria;
-    }
-
-    public void setIdCategoria(Integer idCategoria) {
-        this.idCategoria = idCategoria;
-    }
-
-    public String getNombreCategoria() {
-        return nombreCategoria;
-    }
-
-
-    public void setNombreCategoria(String nombreCategoria) {
-        this.nombreCategoria = nombreCategoria;
-    }
-
-    public String getDescripcionCategoria() {
-        return descripcionCategoria;
-    }
-
-    public void setDescripcionCategoria(String descripcionCategoria) {
-        this.descripcionCategoria = descripcionCategoria;
-    }
-
 }
