cad8f51176caafda6646092ec5a5ef44dfa23dfc
duarteeros23 <eduarte-2025239@kinal.edu.gt>
2026-03-03 10:43:18 -0600

    Cambio de formato de fecha

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/entity/Usuario.java
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/entity/Usuario.java
@@ -3,9 +3,11 @@ package com.proyecto.Learnia.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usuario", uniqueConstraints = {
@@ -22,21 +24,24 @@ public class Usuario {
     private Long idUsuario;
 
     @NotBlank(message = "El nombre del usuario no puede estar vacio")
+    @Size(min = 2, max = 100, message = "Numero de caracteres invalido en el nombre")
     @Column(name = "username")
     private String nombreUsuario;
 
 
     @NotBlank(message = "El correo no puede estar vacio")
+    @Size(min = 5, max = 100, message = "Numero de caracteres invalido en el correo" )
     @Column(name = "correo_usuario", unique = true)
     private String correoUsuario;
 
     @NotBlank(message = "La contraseña no puede estar vacia")
+    @Size(min = 8, max = 100, message = "Numero de caracteres invalido en la contraseña")
     @Column(name = "contrasena")
     private String contrasenaUsuario;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "fecha_registro")
-    private LocalDate fechaRegistro;
+    private LocalDateTime fechaRegistro;
 
 
     @Enumerated(EnumType.STRING)
@@ -50,7 +55,7 @@ public class Usuario {
         this.nombreUsuario = nombreUsuario;
         this.correoUsuario = correoUsuario;
         this.contrasenaUsuario = contrasenaUsuario;
-        this.fechaRegistro = LocalDate.now();
+        this.fechaRegistro = LocalDateTime.now();
     }
 
     public  Usuario(){
