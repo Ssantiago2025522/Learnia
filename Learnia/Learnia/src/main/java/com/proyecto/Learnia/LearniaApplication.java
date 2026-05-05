4b05b1863e744a123cf3c595d3a5b58c8d8f2355
jbolanos-2024525 <jbolanos-2024525@kinal.edu.gt>
2026-05-05 09:30:49 -0600

    Correccion en vista de usuarios

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/LearniaApplication.java
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/LearniaApplication.java
@@ -1,9 +1,11 @@
 package com.proyecto.Learnia;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class        LearniaApplication implements CommandLineRunner {
 
@@ -13,6 +15,6 @@ public class        LearniaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Api funcionando de manera correcta");
+		log.info("Api funcionando de manera correcta");
 	}
 }
