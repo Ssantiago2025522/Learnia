a1e54cf8199a4ab3c4ed2470e8a0b6e64c0fc86d
jbolanos-2024525 <jbolanos-2024525@kinal.edu.gt>
2026-05-05 09:13:12 -0600

    Agregar logo al login y al register

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/LearniaApplication.java
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/LearniaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearniaApplication implements CommandLineRunner {
+public class        LearniaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearniaApplication.class, args);
