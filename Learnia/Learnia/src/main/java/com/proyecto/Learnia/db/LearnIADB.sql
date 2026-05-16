c9b09a783ae3523ea367e3f5ac12bbfd7bfcadaf
Humberto Alessandro Perez Gonzalez <hperez-2022161@kinal.edu.gt>
2026-05-15 19:14:54 -0600

    Cambios en la BD

--- Learnia/Learnia/src/main/java/com/proyecto/Learnia/db/LearnIADB.sql
+++ Learnia/Learnia/src/main/java/com/proyecto/Learnia/db/LearnIADB.sql
@@ -3,104 +3,104 @@ create database Learnia_db_in5bm;
 use Learnia_db_in5bm;
 
 create table usuario(
-	id_usuario bigint  auto_increment  primary key,
-    nombre_usuario varchar(100) not null,
-    correo_usuario varchar(100) not null unique,
-    contrasena varchar(255) not null,
-	fecha_registro datetime,
-    rol enum("ADMIN", "MODERADOR", "ESTUDIANTE") not null,
-    foto varchar(255) null
+                        id_usuario bigint  auto_increment  primary key,
+                        nombre_usuario varchar(100) not null,
+                        correo_usuario varchar(100) not null unique,
+                        contrasena varchar(255) not null,
+                        fecha_registro datetime,
+                        rol enum("ADMIN", "MODERADOR", "ESTUDIANTE") not null,
+                        foto varchar(255) null
 );
 
 update Usuario set rol = "ADMIN" where id_usuario = 4;
 
 create table categoria(
-    id_categoria bigint auto_increment primary key,
-    nombre varchar(100) not null,
-    descripcion text
+                          id_categoria bigint auto_increment primary key,
+                          nombre varchar(100) not null,
+                          descripcion text
 );
 
 create table Recurso(
-    id_recurso bigint auto_increment primary key,
-    titulo_recurso varchar(200) not null,
-    descripcion_recurso text,
-    tipo_recurso enum("documento" , "presentacion" , "audio") not null,
-    url_archivo varchar(255) not null,
-    fecha_subida datetime default current_timestamp,
-    id_usuario bigint not null,
-    id_categoria bigint not null,
-    constraint fk_id_usuario
-    foreign key (id_usuario)
-    references usuario(id_usuario),
-    constraint fk_id_categoria
-    foreign key (id_categoria)
-    references categoria(id_categoria)
-    On delete cascade
+                        id_recurso bigint auto_increment primary key,
+                        titulo_recurso varchar(200) not null,
+                        descripcion_recurso text,
+                        tipo_recurso enum("DOCUMENTO" , "PRESENTACION" , "AUDIO") not null,
+                        url_archivo varchar(255) not null,
+                        fecha_subida datetime default current_timestamp,
+                        id_usuario bigint not null,
+                        id_categoria bigint not null,
+                        constraint fk_id_usuario
+                            foreign key (id_usuario)
+                                references usuario(id_usuario),
+                        constraint fk_id_categoria
+                            foreign key (id_categoria)
+                                references categoria(id_categoria)
+                                On delete cascade
 );
 
 create table pregunta(
-    id_pregunta bigint auto_increment primary key,
-    titulo varchar(200) not null,
-    descripcion text not null,
-    fecha_publicacion datetime default current_timestamp,
-    id_usuario bigint not null,
-    id_categoria bigint not null,
-    constraint fk_id_usuarios
-    foreign key (id_usuario)
-    references usuario(id_usuario),
-    constraint fk_id_categorias
-    foreign key (id_categoria)
-    references categoria(id_categoria)
-    on delete cascade
+                         id_pregunta bigint auto_increment primary key,
+                         titulo varchar(200) not null,
+                         descripcion text not null,
+                         fecha_publicacion datetime default current_timestamp,
+                         id_usuario bigint not null,
+                         id_categoria bigint not null,
+                         constraint fk_id_usuarios
+                             foreign key (id_usuario)
+                                 references usuario(id_usuario),
+                         constraint fk_id_categorias
+                             foreign key (id_categoria)
+                                 references categoria(id_categoria)
+                                 on delete cascade
 );
 
 create table respuesta(
-    id_respuesta bigint auto_increment primary key,
-    contenido text not null,
-    fecha_respuesta datetime default current_timestamp,
-    id_usuario bigint not null,
-    id_pregunta bigint not null,
-    constraint fk_respuesta_usuario
-    foreign key (id_usuario)
-    references usuario(id_usuario)
-    on delete cascade,
-    constraint fk_respuesta_pregunta
-    foreign key (id_pregunta)
-    references pregunta(id_pregunta)
-    on delete cascade
+                          id_respuesta bigint auto_increment primary key,
+                          contenido text not null,
+                          fecha_respuesta datetime default current_timestamp,
+                          id_usuario bigint not null,
+                          id_pregunta bigint not null,
+                          constraint fk_respuesta_usuario
+                              foreign key (id_usuario)
+                                  references usuario(id_usuario)
+                                  on delete cascade,
+                          constraint fk_respuesta_pregunta
+                              foreign key (id_pregunta)
+                                  references pregunta(id_pregunta)
+                                  on delete cascade
 );
 
 Create table Comentario(
-    id_comentario bigint auto_increment primary key,
-    contenido text not null,
-    fecha datetime default current_timestamp,
-    id_usuario bigint not null,
-    id_recurso bigint null,
-    id_respuesta bigint null,
-    constraint fk_usuario
-    foreign key (id_usuario)
-    references usuario(id_usuario),
-    constraint fk_respuesta
-    foreign key (id_respuesta)
-    references respuesta(id_respuesta),
-    constraint fk_recurso
-    foreign key (id_recurso)
-    references recurso(id_recurso)
-    on delete cascade
+                           id_comentario bigint auto_increment primary key,
+                           contenido text not null,
+                           fecha datetime default current_timestamp,
+                           id_usuario bigint not null,
+                           id_recurso bigint null,
+                           id_respuesta bigint null,
+                           constraint fk_usuario
+                               foreign key (id_usuario)
+                                   references usuario(id_usuario),
+                           constraint fk_respuesta
+                               foreign key (id_respuesta)
+                                   references respuesta(id_respuesta),
+                           constraint fk_recurso
+                               foreign key (id_recurso)
+                                   references recurso(id_recurso)
+                                   on delete cascade
 );
 
 Create table voto(
-    id_voto bigint auto_increment primary key,
-    tipo enum('like', 'dislike') not null,
-    id_usuario bigint not null,
-    id_respuesta bigint not null,
-    constraint fk_idusuario
-    foreign key (id_usuario)
-    references usuario(id_usuario),
-    constraint fk_id_respuesta
-    foreign key (id_respuesta)
-    references respuesta(id_respuesta)
-    on delete cascade
+                     id_voto bigint auto_increment primary key,
+                     tipo enum('like', 'dislike') not null,
+                     id_usuario bigint not null,
+                     id_respuesta bigint not null,
+                     constraint fk_idusuario
+                         foreign key (id_usuario)
+                             references usuario(id_usuario),
+                     constraint fk_id_respuesta
+                         foreign key (id_respuesta)
+                             references respuesta(id_respuesta)
+                             on delete cascade
 );
 
 INSERT INTO usuario (nombre_usuario, correo_usuario, contrasena, fecha_registro, rol, foto)
@@ -133,3 +133,6 @@ VALUES
     ('¿Cuál es la diferencia entre una lista y un arreglo en Java?',
      'En mi clase de programación usamos ArrayList y arreglos normales. Quiero saber cuándo usar cada uno y cuáles son sus ventajas y desventajas.',
      NOW(), 2, 3);
+
+select * from Recurso;
+select * from usuario;
